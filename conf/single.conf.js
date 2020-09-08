05f6988e8829554c24b1ae32f9af7fd5723b70d4
nithyamn <nithya.mn999@gmail.com>
2020-09-08 20:23:29 +0530

    Update single.conf.js

--- conf/single.conf.js
+++ conf/single.conf.js
@@ -1,3 +1,5 @@
+const testname = 'single.conf.js';
+
 exports.config = {
   user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
   key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACC_KEY',
@@ -7,13 +9,23 @@ exports.config = {
     './tests/specs/single_test.js'
   ],
   exclude: [],
+  // services: [
+  //   [
+  //   'browserstack',
+  //     {
+  //       browserstackLocal: false
+  //     }
+  //   ],
+  // ],
+  //The above code causes the same error as Shay is getting (this is for cucumber for chai/mocha this is not needed)
 
   capabilities: [{
     browser: 'Chrome',
-    name: 'single_test',
+    name: testname,
+    project: 'Webdriver IO tests',
     build: 'webdriver-browserstack'
   }],
-
+  
   logLevel: 'warn',
   coloredLogs: true,
   screenshotPath: './errorShots/',
@@ -23,14 +35,47 @@ exports.config = {
   connectionRetryCount: 3,
   host: 'hub.browserstack.com',
 
-  before: function () {
-    var chai = require('chai');
-    global.expect = chai.expect;
-    chai.Should();
-  },
+  // before: function () {
+  //   var chai = require('chai');
+  //   global.expect = chai.expect;
+  //   chai.Should();
+  // },
   framework: 'mocha',
   mochaOpts: {
     ui: 'bdd',
     timeout: 60000
   },
+  reporters: ['browserstack'],
+    reporterOptions: {
+        browserstack: {
+            outputDir: './'
+        }
+    },
+
+  beforeSession: function (config, capabilities, specs) {
+    var path = specs[0];
+    var testname = path.replace(/^.*[\\/]/, '').replace(/.js/, '');
+    var projectname = `_test_${new Date().toLocaleDateString()}`;
+
+    //console.log("specs:"+testname);
+    capabilities.name = testname;
+    capabilities.project = projectname;
+  },
+  beforeTest: function (test, context) {
+        var testName = "\"********"+test.title+"*******\";"
+        browser.execute(testName)
+        console.log('--- Running test: ' + testName);
+  },
+
+  // afterTest: function (test) {
+  //       const projectname = `test_${new Date().toLocaleDateString()}`;
+  //       var request = require('request');
+  //       const sessionid = browser.sessionId;
+  //       console.log("Session ID printed: "+sessionid);
+  //       request({
+  //           uri: "https://"+this.user+":"+this.key+"@api.browserstack.com/automate/sessions/"+sessionid+".json", 
+  //           method:"PUT", 
+  //           form:{"name":"updated_sessionname","status":"passed","reason":""}
+  //       })
+  //   }
 }
